6acaf9f2574170fefcd65f176009e3154b13b4a6
vadim <vadimm87@mail.ru>
2026-06-10 21:29:51 +0300

    исправить замечание для static_data.py и test_bun.py

--- data/static_data.py
+++ data/static_data.py
@@ -1,12 +1,11 @@
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
-BUN_POSITIVE_DATA = [
-    ("булочка", 50.0),
-    ("Черный рыцарь", 100.5),
-    ("", 0.0),          
-    ("Black burger", 0.01),   
-    ("Пышка Премьер", 999999.99) 
-]
+# Для Bun: тестируем имя (цена фиксирована) и цену (имя фиксировано)
+BUN_NAMES = ["булочка", "Черный рыцарь", "", "Black burger", "Пышка Премьер"]
+FIXED_BUN_PRICE = 100.0
+
+BUN_PRICES = [0.0, 0.01, 50.0, 100.5, 999999.99]
+FIXED_BUN_NAME = "Тестовая булочка"
 
 INGREDIENT_POSITIVE_DATA = [
     (INGREDIENT_TYPE_SAUCE, "сырный", 100),
